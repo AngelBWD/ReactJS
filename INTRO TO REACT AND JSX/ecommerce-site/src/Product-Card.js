169f4e0865b6452be078cef28ec996c52f6eae3a
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2019-11-01 00:22:24 +0200

    Add files via upload

---
+++ INTRO TO REACT AND JSX/ecommerce-site/src/Product-Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+function ProductCard() {
+  return (
+<div>
+    <h3>Product 1</h3>
+    <div>
+        <span>Price 20$</span>
+        <p>Test Product</p>
+    </div>
+</div>
+    )
+}
+
+export default ProductCard;
