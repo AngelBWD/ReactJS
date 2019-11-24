d0fd08152214ada37ac677432fda514eea999c88
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2019-11-24 11:20:48 +0200

    Add files via upload

--- FORMS/src/checkout-page/index.js
+++ FORMS/src/checkout-page/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from '../header';
 import CheckoutForm from '../checkout-form'
 
-const CheckoutPage = () => {
-    return(
-        <div>
-            <Header/>
-            <CheckoutForm />
-        </div> 
-    )
+class CheckoutPage extends Component  {
+    // componentDidMount() {                    //Връзка с другите страници!
+    //     setInterval(() => {
+    //         this.props.history.push({
+    //             pathname: '/',
+    //             state: {
+    //                 loggedIn: true,
+    //                 username: 'Val'
+    //             }
+    //         })  
+    //     }, 3000);
+    // }
+    render() {
+        return(
+            <div>
+                <Header/>
+                <CheckoutForm />
+            </div> 
+        )
+    }
+    
 }
 
 export default CheckoutPage
