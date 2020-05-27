d8217e686cbb9ba2ddbdf50e92ceba967a820563
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2020-05-27 07:51:43 +0300

    Add files via upload

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ProductCard from './Product-Card';
 
+
+function Img() {
+return (
+  <img src={logo} className="App-logo" alt="logo"/>
+  )
+}
 function App() {
+  const firstNumber = 1;
+  const secondNumber = 3;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Img/>
+        <ProductCard/>
       </header>
     </div>
   );
