e3507a043df233a7e218f3620900a4eb9da173f3
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2020-05-30 10:17:32 +0300

    Add files via upload

---
+++ my-app/src/product/Product.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Product = ({product, Price}) => {
+    return (
+        <div className="product-tile">
+        <img className="product-image" src={product.image} alt={product.title} />
+        <div>
+          <span className="product-brand">{product.brand}</span>
+          <span className="product-title">{product.title}</span>
+          <Price price={product.price} />
+        </div>
+      </div>
+    )
+}
+
+export default Product
