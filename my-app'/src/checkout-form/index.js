ea4f91b3b2334dd367ad642461a7440410f96ca1
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2020-06-10 07:50:52 +0300

    Add files via upload

--- my-app'/src/checkout-form/index.js
+++ my-app'/src/checkout-form/index.js
@@ -10,23 +10,92 @@ class CheckoutForm extends Component {
             city: '',
             iban: '',
             gender: '',
-            terms: false
+            terms: false,
+            error: null
         }
     }
+
+
+    changeUsername = e => {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    changeAddress = e => {
+        this.setState({
+            address: e.target.value
+        })
+    }
+
+    changeIban = e => {
+        this.setState({
+            iban: e.target.value
+        })
+    }
+
+    changeCity = e => {
+        this.setState({
+            city: e.target.value
+        })
+    }
+
+    changeTerm = e => {
+        this.setState({
+            terms: e.target.checked
+        })
+    }
+
+    changeGender = e => {
+        this.setState({
+            gender: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+       e.preventDefault()
+       const { 
+        username,
+        address, 
+        city, 
+        iban, 
+        gender, 
+        terms 
+    } = this.state
+
+        if(username ==='') {
+            this.setState({
+                error: 'Username is mandatory, bro!'
+            })
+        } else {
+            console.log(this.state);     
+        }
+    }
+
     render() {
+        const { 
+            username,
+            address, 
+            city, 
+            iban, 
+            error, 
+            terms 
+        } = this.state
+
         return (
-            <form>
+            <form onSubmit={this.handleSubmit} >
                 <div>
                     <label htmlFor='username'>Username:</label>
-                    <input type='text' id='username'></input>
+                    <input type='text' onChange={this.changeUsername} value={username} id='username'></input>
                 </div>
                 <div>
                     <label htmlFor='address'>Address:</label>
-                    <input type='text' id='address'></input>
+                    <input type='text' onChange={this.changeAddress} value={address} id='address'></input>
                 </div>
                 <div>
                     <label htmlFor='city'>City:</label>
-                    <select>
+                    <select onChange={this.changeCity} value={city}>
+                        <option value=''>Choose...</option>
                         <option value='Sofia'>Sofia</option>
                         <option value='Plovdiv'>Plovdiv</option>
                         <option value='Sliven'>Sliven</option>
@@ -35,20 +104,27 @@ class CheckoutForm extends Component {
                 </div>
                 <div>
                     <label htmlFor='iban'>IBAN:</label>
-                    <input type='text' id='iban'></input>
+                    <input onChange={this.changeIban} type='text' value={iban} id='iban'></input>
                 </div>
                 <div>
                     <label htmlFor='terms'>Terms & Conditions:</label>
-                    <input type='checkbox' />
+                    <input type='checkbox' onChange={this.changeTerm} selected={terms}/>
                 </div>
                 <div>
                     <label htmlFor='gender'>Gender: </label>
-                    Male:<input name='gender' type='radio' value='male'/>
-                    Woman:<input name='gender' type='radio' value='woman'/>
-                    Other:<input name='gender' type='radio' value='other'/>
+                    Male:<input onChange={this.changeGender} name='gender' type='radio' value='male'/>
+                    Woman:<input onChange={this.changeGender} name='gender' type='radio' value='woman'/>
+                    Other:<input onChange={this.changeGender} name='gender' type='radio' value='other'/>
                 </div>
 
                 <button type='submit'>Submit</button>
+                {error ? 
+                    (
+                            <div>
+                                {error}
+                            </div>
+                    ) : null
+                }
             </form>
         )
     }
