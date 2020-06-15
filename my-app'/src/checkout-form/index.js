8af8b7d9446537b4946631004d24dc20a56570a1
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2020-06-15 07:45:56 +0300

    Add files via upload

--- my-app'/src/checkout-form/index.js
+++ my-app'/src/checkout-form/index.js
@@ -1,7 +1,83 @@
 import React, { Component } from 'react'
 import UsernameInput from './username-input'
+import { Form, Field } from 'react-final-form'
 
 class CheckoutForm extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state={
+            username: '',
+            address: '',
+            city: '',
+            iban: '',
+            gender: '',
+            terms: false,
+            error: null
+        }
+    }
+
+
+    changeUsername = e => {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    changeAddress = e => {
+        this.setState({
+            address: e.target.value
+        })
+    }
+
+    changeIban = e => {
+        this.setState({
+            iban: e.target.value
+        })
+    }
+
+    changeCity = e => {
+        this.setState({
+            city: e.target.value
+        })
+    }
+
+    changeTerm = e => {
+        this.setState({
+            terms: e.target.checked
+        })
+    }
+
+    changeGender = e => {
+        this.setState({
+            gender: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+       e.preventDefault()
+       const { 
+        username,
+        address, 
+        city, 
+        iban, 
+        gender, 
+        terms 
+    } = this.state
+
+        if(username ==='') {
+            this.setState({
+                error: 'Username is mandatory, bro!'
+            })
+        } else {
+            this.setState({
+                error: null
+            })     
+            console.log(this.state);
+            
+        }
+    }
     
 
     render() {
@@ -11,26 +87,19 @@ class CheckoutForm extends Component {
             city, 
             iban, 
             error, 
-            terms,
-            handleSubmit,
-            changeAddress,
-            changeUsername,
-            changeCity,
-            changeGender,
-            changeIban,
-            changeTerm
-        } = this.props
+            terms
+        } = this.state
 
         return (
-            <form onSubmit={handleSubmit} >
-                <UsernameInput username={username} changeUsername={changeUsername}/>
+            <form onSubmit={this.handleSubmit} >
+                <UsernameInput username={username} changeUsername={this.changeUsername}/>
                 <div>
                     <label htmlFor='address'>Address:</label>
-                    <input type='text' onChange={changeAddress} value={address} id='address'></input>
+                    <input type='text' onChange={this.changeAddress} value={address} id='address'></input>
                 </div>
                 <div>
                     <label htmlFor='city'>City:</label>
-                    <select onChange={changeCity} value={city}>
+                    <select onChange={this.changeCity} value={city}>
                         <option value=''>Choose...</option>
                         <option value='Sofia'>Sofia</option>
                         <option value='Plovdiv'>Plovdiv</option>
@@ -40,17 +109,17 @@ class CheckoutForm extends Component {
                 </div>
                 <div>
                     <label htmlFor='iban'>IBAN:</label>
-                    <input onChange={changeIban} type='text' value={iban} id='iban'></input>
+                    <input onChange={this.changeIban} type='text' value={iban} id='iban'></input>
                 </div>
                 <div>
                     <label htmlFor='terms'>Terms & Conditions:</label>
-                    <input type='checkbox' onChange={changeTerm} selected={terms}/>
+                    <input type='checkbox' onChange={this.changeTerm} selected={terms}/>
                 </div>
                 <div>
                     <label htmlFor='gender'>Gender: </label>
-                    Male:<input onChange={changeGender} name='gender' type='radio' value='male'/>
-                    Woman:<input onChange={changeGender} name='gender' type='radio' value='woman'/>
-                    Other:<input onChange={changeGender} name='gender' type='radio' value='other'/>
+                    Male:<input onChange={this.changeGender} name='gender' type='radio' value='male'/>
+                    Woman:<input onChange={this.changeGender} name='gender' type='radio' value='woman'/>
+                    Other:<input onChange={this.changeGender} name='gender' type='radio' value='other'/>
                 </div>
 
                 <button type='submit'>Submit</button>
--- my-app'/src/checkout-page/index.js
+++ my-app'/src/checkout-page/index.js
@@ -3,127 +3,15 @@ import Header from '../header'
 import CheckoutForm from '../checkout-form'
 
 
-class CheckoutPage extends Component {
-
-    constructor(props) {
-        super(props)
-
-        this.state={
-            username: '',
-            address: '',
-            city: '',
-            iban: '',
-            gender: '',
-            terms: false,
-            error: null
-        }
-    }
-
-
-    changeUsername = e => {
-        this.setState({
-            username: e.target.value
-        })
-    }
-
-    changeAddress = e => {
-        this.setState({
-            address: e.target.value
-        })
-    }
-
-    changeIban = e => {
-        this.setState({
-            iban: e.target.value
-        })
-    }
-
-    changeCity = e => {
-        this.setState({
-            city: e.target.value
-        })
-    }
-
-    changeTerm = e => {
-        this.setState({
-            terms: e.target.checked
-        })
-    }
-
-    changeGender = e => {
-        this.setState({
-            gender: e.target.value
-        })
-    }
-
-    handleSubmit = e => {
-       e.preventDefault()
-       const { 
-        username,
-        address, 
-        city, 
-        iban, 
-        gender, 
-        terms 
-    } = this.state
-
-        if(username ==='') {
-            this.setState({
-                error: 'Username is mandatory, bro!'
-            })
-        } else {
-            this.setState({
-                error: null
-            })     
-            console.log(this.state);
-            
-        }
-    }
-
-    // componentDidMount() {
-    //     setTimeout(()=> {
-    //         this.props.history.push({
-    //             pathname: '/',
-    //             state: {
-    //                 loggedIn: true,
-    //                 username: 'Angel'
-    //             }
-    //         })
-
-    //     },3000);
-    // }
-    render(){
-        const { 
-            username,
-            address, 
-            city, 
-            iban, 
-            error, 
-            terms 
-        } = this.state
+const CheckoutPage = () => {
 
         return (
             <div>
                 <Header />
-                <CheckoutForm 
-                username={username} 
-                address={address} 
-                city={city}
-                iban={iban}
-                error={error}
-                terms={terms}
-                handleSubmit={this.handleSubmit}
-                changeAddress={this.changeAddress}
-                changeUsername={this.changeUsername}
-                changeCity={this.changeCity}
-                changeGender={this.changeGender}
-                changeIban={this.changeIban}
-                changeTerm={this.changeTerm}
-                />
+                <CheckoutForm />
             </div>
         )
-
-    }
 }
+
 
 export default CheckoutPage 
