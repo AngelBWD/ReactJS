af0f4df4539759d75d49662127faaf8df2e746ff
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2020-06-22 07:28:30 +0300

    Add files via upload

--- project/src/App.js
+++ project/src/App.js
@@ -1,51 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 class App extends Component {
+  constructor ( props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+  componentDidMount() {
+   this.timer = setInterval( this.incrementCounter, 1000)
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('I just updated');
+    
+    if(prevState.counter === 50){
+      this.setState({ counter: 0});
+    }
+  }
+
+  componentWillMount() {
+    clearInterval( this.timer );
+  }
+
+  incrementCounter = () =>  {
+    this.setState( () => {
+      return { counter: this.state.counter++}
+    });
+  }
+
   render() { 
     return (
       <div className='App'>
-        <Header title='React'/>
-        <Content color='red'>Това е this.props.children</Content>
-        <Content color='blue'>Аз съм син</Content>
-        <Content color='green'>Аз съм зелен</Content>
-        <Footer />
+       <h1>{ this.state.counter }</h1>
       </div>
     );
   }
 }
 
-class Header extends Component {
-  render() { 
-  return <h1>Курс по {this.props.title}</h1>;
-  }
-}
-
-class Content extends Component {
-  render() { 
-    return (
-      <div style={ {color: this.props.color} }>
-        <br />
-        <i>{this.props.children}</i>
-        <br />
-        <small>Публикувано на { new Date().toLocaleTimeString() }</small>
-      </div>
-    );
-  }
-}
-
-class Footer extends Component {
-  render() { 
-  return ( 
-      <>
-          <br />
-          <hr />
-          <small>Всички права запазени</small>;
-      </>
-    )
-  }
-}
 
 export default App;
