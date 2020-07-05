30eb0e89eb7cb90f263829db0075da31896ed664
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2020-07-05 15:49:40 +0300

    Add files via upload

--- origamy/src/App/App.js
+++ origamy/src/App/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import './App.css';
-import Navigation from '../Navigation/Navigation'
-import Aside from '../Aside/Aside'
-import Posts from '../Posts/Posts'
-import Main from './Main/Main'
-import Footer from '../Footer/Footer'
-import Loader from '../Loader/Loader'
+import Navigation from '../Navigation/Navigation';
+import Aside from '../Aside/Aside';
+import Posts from '../publications/Posts/Posts';
+import Main from './Main/Main';
+import Footer from '../Footer/Footer';
+import Loader from './Loader/Loader';
+import CreatePost from '../publications/CreatePost/CreatePosts';
+import Profile from '../Profile/Profile';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    
-    <div className="App">
-      <Loader local={true} isLoading={false} />
-      <Navigation/>
-      <div className='Container'>
-        <Aside />
-        <Main title='Hello!'>
-          <Posts />
-        </Main>
-      </div>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+        <div className="App">
+          <Loader local={true} isLoading={false} />
+          <Navigation/>
+          <div className='Container'>
+            <Aside />
+            <Main title='Hello!'>
+              <Switch>
+                <Route path='/' exact component={Posts}/>
+                <Route path='/create-post'  component={CreatePost}/>
+                <Route path='/profile'  component={Profile}/>
+                <Route path='/login'  component={Login}/>
+                <Route path='/register'  component={Register}/>
+              </Switch>
+            </Main>
+          </div>
+          <Footer/>
+        </div>
+    </BrowserRouter>
   );
 }
 
