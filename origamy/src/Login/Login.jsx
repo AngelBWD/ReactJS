e7698ccfed233e46513351c24debb58efb7a24ff
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2020-07-08 23:23:35 +0300

    Add files via upload

---
+++ origamy/src/Login/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../shared/styles/LoginAndRegister.css'
+export default function Login() {
+    return (
+        <form className='Login'>
+            <div className='form-control'>
+                <label>Username</label>
+                <input type="text"/>
+            </div>
+            <div className='form-control'>
+                <label>Password</label>
+                <input type="password"/>
+            </div>
+            <div className='form-control'>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
