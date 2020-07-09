e8c6ef0134c7d6a6fc59a33138f509e7398cfbd7
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2020-07-09 07:57:14 +0300

    Add files via upload

---
+++ origamy/src/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './NotFound.css'
+export default function NotFound() {
+    return (
+        <div className='FourOFour'>
+            <img src="" alt="404"/>
+        </div>
+    )
+}
+
+
