56463331a77f509fce9226b6abdab70d02ef4441
Angel Beshirov <47317380+AngelBWD@users.noreply.github.com>
2020-07-09 07:58:27 +0300

    Add files via upload

--- origamy/src/App/App.js
+++ origamy/src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 import './App.css';
 import Navigation from '../Navigation/Navigation';
 import Aside from '../Aside/Aside';
@@ -9,8 +9,14 @@ import Loader from './Loader/Loader';
 import CreatePost from '../publications/CreatePost/CreatePosts';
 import Profile from '../Profile/Profile';
 import Login from '../Login/Login';
+import NotFound from '../NotFound/NotFound';
 import Register from '../Register/Register';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+function render(title, Cmp) {
+    return <Main title={title}><Cmp /></Main>
+  };
+
 
 function App() {
   return (
@@ -20,15 +26,27 @@ function App() {
           <Navigation/>
           <div className='Container'>
             <Aside />
-            <Main title='Hello!'>
               <Switch>
-                <Route path='/' exact component={Posts}/>
-                <Route path='/create-post'  component={CreatePost}/>
-                <Route path='/profile'  component={Profile}/>
-                <Route path='/login'  component={Login}/>
-                <Route path='/register'  component={Register}/>
+                <Route path='/' exact >
+                    <Main title='Posts'><Posts /></Main>
+                </Route>
+                <Route path='/create-post'>
+                    <Main title='Create Post'><CreatePost /></Main>
+                </Route>
+                <Route path='/profile'>
+                   <Main title='Profile'><Profile /></Main>
+                </Route>
+                <Route path='/login'>
+                    <Main title='Login'><Login /></Main>
+                </Route>
+                <Route path='/register'>
+                    <Main title='Register'><Register /></Main>
+                </Route>
+                <Route path='*'>
+                    <Main title='Not Found'><NotFound /></Main>
+                </Route>
               </Switch>
-            </Main>
+            
           </div>
           <Footer/>
         </div>
